a6823013569f6e213f43102de10c0d403bc51b84
Luis Correa <correaleyval@gmail.com>
2020-04-05 18:48:33 -0400

    logo in graph test

--- imagemark.py
+++ imagemark.py
@@ -11,13 +11,13 @@ def watermark_text(input_image_path,
 
     wl, hl = logo.size
 
-    photo.paste(logo, (10,H-hl))
+    photo.paste(logo, (10,H-hl-20))
 
     # make the image editable
     drawing = ImageDraw.Draw(photo)
     black = (112, 128, 144)
     font = ImageFont.truetype("./font.ttf", 15)
     w, h = drawing.textsize(text)
-    drawing.text((wl+15,H-h-20), text, fill=black, font=font)
+    drawing.text((wl+15,H-h-25), text, fill=black, font=font)
     #photo.show()
     photo.save(output_image_path)
