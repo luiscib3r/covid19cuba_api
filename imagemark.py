86b86c20a738e9735ec7ee4407f4037bb8951390
Luis Correa <correaleyval@gmail.com>
2020-04-05 17:29:55 -0400

    url in graph test

--- imagemark.py
+++ imagemark.py
@@ -8,9 +8,8 @@ def watermark_text(input_image_path,
     photo = Image.open(input_image_path)
     # make the image editable
     drawing = ImageDraw.Draw(photo)
-    black = (3, 8, 12)
-    #font = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 40)
-    #drawing.text(pos, text, fill=black, font=font)
-    drawing.text(pos, text, fill=black)
+    black = (112, 128, 144)
+    font = ImageFont.truetype("./font.ttf", 12)
+    drawing.text(pos, text, fill=black, font=font)
     photo.show()
     photo.save(output_image_path)
