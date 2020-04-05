eb2d282554288d33db3e8efb3d7402d742d15adc
Luis Correa <correaleyval@gmail.com>
2020-04-05 17:40:36 -0400

    url in graph test

--- imagemark.py
+++ imagemark.py
@@ -6,10 +6,12 @@ def watermark_text(input_image_path,
                    output_image_path,
                    text, pos):
     photo = Image.open(input_image_path)
+    W, H = photo.size
     # make the image editable
     drawing = ImageDraw.Draw(photo)
     black = (112, 128, 144)
     font = ImageFont.truetype("./font.ttf", 12)
-    drawing.text(pos, text, fill=black, font=font)
+    w, h = drawing.textsize(text)
+    drawing.text(((W-w)/2,0), text, fill=black, font=font)
     photo.show()
     photo.save(output_image_path)
