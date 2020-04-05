771b3c3f143f24223ac5f34f931171229e50d1ab
Luis Correa <correaleyval@gmail.com>
2020-04-05 19:03:24 -0400

    adjust

--- imagemark.py
+++ imagemark.py
@@ -11,13 +11,13 @@ def watermark_text(input_image_path,
 
     wl, hl = logo.size
 
-    photo.paste(logo, (5,H-hl-10))
+    photo.paste(logo, (5,H-hl-7))
 
     # make the image editable
     drawing = ImageDraw.Draw(photo)
     black = (112, 128, 144)
     font = ImageFont.truetype("./font.ttf", 15)
     w, h = drawing.textsize(text)
-    drawing.text((wl+5,H-h-12), text, fill=black, font=font)
+    drawing.text((wl+10,H-h-12), text, fill=black, font=font)
     #photo.show()
     photo.save(output_image_path)
